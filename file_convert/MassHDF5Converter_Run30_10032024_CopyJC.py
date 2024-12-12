a61b4395de9e8a33f997a5b4880aef649b010716
EclipsedClaw <jianchengzengkid@sina.cn>
2024-12-12 14:50:37 -0500

    upload for jon to debug

--- file_convert/MassHDF5Converter_Run30_10032024_CopyJC.py
+++ file_convert/MassHDF5Converter_Run30_10032024_CopyJC.py
@@ -109,7 +109,7 @@ print(f'Accessing data from: {data_path}')
 
 
 # Access each acquisition directory to produce HDF5 files from binary data
-acq_id = ''
+acq_id = 'Pedestal'
 #acq_id = 'LArSiPMTesting'  # Define identifier for acquisition (Use common identifier to convert all binaries from given run)
 acq_ct = 1
 for entry in data_dir_obj:

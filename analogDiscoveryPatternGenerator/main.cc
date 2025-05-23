aa1eaa2a9df745cb5fe44ab347ae201a27841cf2
Madhav Kapa <madhavkapa16@gmail.com>
2025-05-23 18:27:36 -0400

    added PPS pulse and pulse train for the start signal

--- analogDiscoveryPatternGenerator/main.cc
+++ analogDiscoveryPatternGenerator/main.cc
@@ -1,84 +1,41 @@
-#include "AnalogDiscoveryIO.hh"
-#include <chrono>
+#include "WF_SDK/WF_SDK.h"
 #include <iostream>
-#include <thread>
-#include <unistd.h>
-#include <poll.h>
+#include <string>
+#include <fstream>
+#include <vector>
 
-using namespace gramsballoon;
+using namespace wf;
 
+/* ----------------------------------------------------- */
 
-bool check_ManualTrigger() {
-  struct pollfd cli_input = {};
-  cli_input.fd = 0; // 0 means keyboard
-  cli_input.events = POLLIN;
-  return poll(&cli_input, 1, 0) == 1;
-}
+int main(void)
+{
+  Device::Data *device_data;
 
-bool check_PPS_extTrigger(){
+  device_data = device.open();
 
-return true;
-}
+  // PPS signal 100ns pulse per second
+  wavegen.generate(device_data, 1, wavegen.function.pulse, 0, 1e3, 3.3, 0.01, 0.999, 1 / 1e3, 0, std::vector<double>());
+  FDwfAnalogOutIdleSet(device_data->handle, 0, 0);
+  FDwfAnalogOutConfigure(device_data->handle, 0, true);
 
+  // Train signal 10 100ns pulse per 10ms
+  wavegen.generate(device_data, 2, wavegen.function.pulse, 0, 1e3, 3.3, 0.01, 0.009, 1 / 1e3, 0, std::vector<double>());
+  FDwfAnalogOutIdleSet(device_data->handle, 1, 0);
+  FDwfAnalogOutConfigure(device_data->handle, 1, true);
 
+  // Failed OR gate attempt
+  // FDwfDigitalOutEnableSet(device_data->handle, 0, false);
+  // FDwfDigitalOutEnableSet(device_data->handle, 1, false);
+  // FDwfDigitalOutEnableSet(device_data->handle, 15, true);
+  // FDwfDigitalOutTypeSet(device_data->handle, 1, DwfDigitalOutTypeROM);
+  // FDwfDigitalOutDividerSet(device_data->handle, 15, 1);
+  // FDwfDigitalOutOutputSet(device_data->handle, 15, DwfDigitalOutOutputPushPull);
+  // uint8_t truthTableOR = 0b00001110;
+  // FDwfDigitalOutDataSet(device_data->handle, 15, &truthTableOR, 8);
+  // FDwfDigitalOutConfigure(device_data->handle, true);
 
-int main(int argc, char const *argv[]) {
-  double voltage = 0.0;
-
-  if (argc == 1) {
-    voltage = 0.0;
-  }
-  else if (argc == 2) {
-    try {
-      voltage = std::stod(argv[1]);
-    }
-    catch (const std::invalid_argument &e) {
-      std::cerr << "Invalid argument: " << argv[1] << std::endl;
-      return 1;
-    }
-  }
-  else {
-    std::cerr << "Usage: " << argv[0] << " voltage" << std::endl;
-    return 1;
-  }
-  AnalogDiscoveryIO io;
-  const int ret = io.initialize(); // Detect how many AD2 are connected, and start Commnication with them.
-  if (ret != 0) {
-    return ret;
-  }
-  const int connect = io.connect(0); // Connect to the first AD2
-  //const int connect = io.connect(); // Connect to all AD2
-  if (connect != 0) {
-    return connect;
-  }
-
-
-
-  std::cout << "Waiting for the trigger signal" << std::endl;
-  std::string ManualTrigger, PPSextTrigger, PAT = "PAT", PPS = "PPS";
-
-  while (true){
-
-    while(true){
-      
-      if (check_PPS_extTrigger) {
-        io.setupAnalogOut(0, 0, voltage, PPS); 
-        std::this_thread::sleep_for(std::chrono::seconds(1)); //T2 use trigger input for PPS generation
-      }  
-      if (check_ManualTrigger()) {
-        std::getline(std::cin, ManualTrigger);  
-        std::cout << "Received trigger: " << ManualTrigger << std::endl;
-
-        if (ManualTrigger == "1") {
-          io.setupAnalogOut(0, 1, voltage, PAT); //T1
-
-        }
-      }
-     
-    }
-
-  }
-
+  tools.sleep(10000);
+  device.close(device_data);
   return 0;
 }
-

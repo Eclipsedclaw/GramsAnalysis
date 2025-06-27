38adfc515321344b151766c1f4f022a2e5301bf1
EclipsedClaw <jianchengzengkid@sina.cn>
2025-06-27 10:34:42 -0400

    added default selection for file name and save path

--- file_convert/robin_tree_write.py
+++ file_convert/robin_tree_write.py
@@ -11,11 +11,16 @@ file_path = prompt("Please enter the absolute path to the binary file: ", comple
 print(f"You selected: {file_path}")
 
 
-save_path = prompt("Please enter the absolute path where you would like the root file to be saved: ", completer=completer)
-print(f"You selected: {save_path}")
-tree_name = str(input("Enter the tree name: "))
+save_path = prompt("Please enter the absolute path where you would like the root file to be saved (default same as bin file directory): ", completer=completer)
+if(save_path == ''):
+    save_path = os.path.dirname(file_path)
+print(f'You selected: {save_path}')
 
-save_name = os.path.join(save_path, tree_name+".root")
+tree_name = str(input("Enter the root file name with absolute path (default from source file): "))
+if(tree_name == ''):
+    file_name = os.path.basename(file_path)
+    print("filename is: ", file_name)
+    save_name = os.path.join(save_path, os.path.splitext(file_name)[0]+".root")
 print(save_name)
 
 with open(file_path, "rb") as file:

9a8736aa7ca3c86bca88ca4cb6848838ce73ced4
Drew Ames <dreames21@gmail.com>
2026-02-03 11:47:18 -0500

    Fix offsets in bin file read, add array splitting

--- file_convert/uproot_fileperboard.py
+++ file_convert/uproot_fileperboard.py
@@ -1,99 +1,142 @@
 import uproot
 import numpy as np
 import struct
+import time
 from pathlib import Path
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import PathCompleter
 
-# hardcode data path for testing for now
-#filename = ("/NAS/GAr_TPC_Runs/Run5/GArCombo5cmDrift_Run5_UPS_60ch_TPCHV0_pedestal1_20251022/"
-#           "GArCombo5cmDrift_Run5_UPS_60ch_TPCHV0_pedestal1_20251022_dig2-usb51054_20251022140656-04.bin")
-
 ## field sizes in bytes - in this order
 L_EVENT = 28 # 4 + 8 + 4 + 8 + 4 bytes
 
-
-def header_to_dtype(file_path):
-    """Generate numpy dtype from first event in binary header """
-
+def read_header(file_path, header_len=L_EVENT):
+    """Open file and read first few bytes to determine event structure"""
     path = Path(file_path)
     with open(path, "rb") as file:
-        ## read out a portion of the first event to get channel & sampling info
-        header = file.read(L_EVENT)
-
-    _, _, n_samps, res, n_chans = struct.unpack("<iqiqi", header)
-
+        header = file.read(header_len)
+    _, _, n_samps, res, n_chans = struct.unpack("<IQIQi", header)
     print(f"Read event info from first event in {path.name}:")
     print(f" {n_chans} channels")
     print(f" {n_samps} samples")
     print(f" {res} ns resolution")
+    return n_chans, n_samps
 
+def bin_dtype(n_chans, n_samps):
+    """dtype for reading bin files"""
     return np.dtype([
-        ("event_num", "i4"),
-        ("timestamp", "i8"),
-        ("num_of_samples", "i4"),
-        ("resolution", "i8"),
+        ("event_num",       "u4"),
+        ("timestamp",       "u8"),
+        ("num_of_samples",  "u4"),
+        ("resolution",      "u8"),
         ("num_of_channels", "i4"),
-        ("active_channels", "i2", (n_chans)),
-        ("waveform_data", "f4", (n_chans*n_samps))
+        ("waveforms", [("active_channels", "i2"),
+                       ("waveform_data", "f4", n_samps)], n_chans)
     ])
 
+def intermediate_dtype(n_chans, n_samps):
+    """Intermediate dtype for writing to TTree"""
+    return np.dtype([
+        ("event_num",       "u4"),
+        ("timestamp",       "u8"),
+        ("num_of_samples",  "u4"),
+        ("resolution",      "u8"),
+        ("num_of_channels", "i4"),
+        ("active_channels", "i2", (n_chans)),
+        ("waveform_data",   "f4", (n_chans*n_samps))
+    ])
 
-
-
-
-def bin_to_array(file_path, dtype):
-    """Read a single binary file into numpy array"""
-
-    path = Path(file_path)
-    #print(f"Reading data from {path.name} into numpy array")
-    data = np.fromfile(file_path, dtype=dtype)
-    #print(f"{len(data)} events read")
-
-    return data
-
-
-def bins_to_root(bin_files, root_filename):
-    dtype = header_to_dtype(bin_files[0])
-    ttree_branch_types = {
+def ttree_branch_types(n_chans, n_samps):
+    """Branch names and type definitions for writing to TTree"""
+    return {
         "event_num" :       "i4",
-        "timestamp" :       "i8",
+        "timestamp" :       "u8",
         "num_of_samples" :  "i4",
         "resolution" :      "i4",
         "num_of_channels" : "i4",
-        "active_channels" : dtype["active_channels"],
-        "waveform_data" :   dtype["waveform_data"]
+        "active_channels" : ("i4", n_chans),
+        "waveform_data" :   ("f4", n_chans*n_samps)
     }
-    with uproot.recreate(root_filename) as outfile:
-        outfile.mktree("test_tree", ttree_branch_types, title="test_tree")
-        n_evts = 0
-        all_arrays = []
+
+def header_to_dtype(file_path):
+    """Generate numpy dtype from first event in binary header """
+    path = Path(file_path)
+    with open(path, "rb") as file:
+        header = file.read(L_EVENT)
+    _, _, n_samps, res, n_chans = struct.unpack("<IQIQi", header)
+    print(f"Read event info from first event in {path.name}:")
+    print(f" {n_chans} channels")
+    print(f" {n_samps} samples")
+    print(f" {res} ns resolution")
+    return np.dtype([
+        ("event_num",       "u4"),
+        ("timestamp",       "u8"),
+        ("num_of_samples",  "u4"),
+        ("resolution",      "u8"),
+        ("num_of_channels", "i4"),
+        ("waveforms", [("active_channels", "i2"),
+                       ("waveform_data", "f4", n_samps)], n_chans)
+    ])
+
+def transform_array(data):
+    """Convert from binary file dtype to TTree dtype"""
+    n_evts, n_chans, n_samps = data["waveforms"]["waveform_data"].shape
+    new_dtype = intermediate_dtype(n_chans, n_samps)
+    new_data = np.recarray((n_evts,), dtype=new_dtype)
+    identical_fields = ["event_num", "timestamp", "num_of_samples", "resolution", "num_of_channels"]
+    new_data[identical_fields] = data[identical_fields]
+    new_data["active_channels"] = data["waveforms"]["active_channels"]
+    new_data["waveform_data"] = data["waveforms"]["waveform_data"].reshape((n_evts, n_chans*n_samps))
+    return new_data
+
+def split_array(array, max_bytes=None):
+    """Splits the array into multiple arrays of (approximately equal) size less than max_bytes.
+    If max_bytes is None, or smaller than a single event, maximally split array (one array per
+    event).
+    """
+    n_entries = len(array)
+    n_bytes = array.nbytes
+    if max_bytes is None:
+        splits = n_entries
+    else:
+        splits = n_bytes // max_bytes
+        if splits > n_entries:
+            splits = n_entries
+    return np.array_split(array, int(splits))
+
+def bins_to_root(bin_files, root_path, max_basket_size=None):
+    """Take list of bin file paths and write all data to a single root file with TBaskets of size
+    max_basket_size (in bytes). If max_basket_size is None, then split into one TBasket per event"""
+    start = time.time()
+
+    # Get event structure by reading first event in first bin file
+    n_chans, n_samps = read_header(bin_files[0])
+    dtype = bin_dtype(n_chans, n_samps)
+    n_evts = 0
+    with uproot.recreate(root_path) as outfile:
+        outfile.mktree("test_tree", ttree_branch_types(n_chans, n_samps), title="test_tree")
         for bin in bin_files:
-            data = bin_to_array(bin, dtype)
-            all_arrays.append(data)
-            n_evts += len(data)
-            print(f"Writing {len(data)} events from {bin.name} to {root_filename.name}")
-            #outfile["test_tree"].extend(data)
-        all_data = np.concatenate(all_arrays)
-        outfile["test_tree"].extend(all_data)
-        print(f"{n_evts} total events written to {root_filename.name}")
-        
+            data = np.fromfile(bin, dtype=dtype)
+            print(f"Writing {len(data)} events from {bin.name} to {root_path.name}")
+            split_data = split_array(data, max_basket_size)
+            for d in split_data:
+                outfile["test_tree"].extend(transform_array(d))
+                n_evts += len(d)
+                print(n_evts)
+    end = time.time()
+    print(f"{n_evts} total events written to {root_path} in {end-start:0.2f} seconds")
 
 if __name__ == "__main__":
 
     completer = PathCompleter()
-
-    bin_dir = prompt("Please enter the absolute path to the binary file directory: ", completer=completer)
-    root_file_name = str(input("Please enter a file name for the new root file: "))
-
-    if not root_file_name.endswith(".root"):
-        root_file_name += ".root"
-
+    bin_dir = prompt("Please enter the absolute path to the binary file directory: ",
+                     completer=completer)
     bin_dir_path = Path(bin_dir)
     bin_files = sorted(bin_dir_path.glob("*.bin"))
+
+    print("ROOT file will be saved in the same directory as bin files")
+    root_file_name = str(input("Please enter a file name for the new ROOT file: "))
+    if not root_file_name.endswith(".root"):
+        root_file_name += ".root"
     root_file_path = bin_dir_path / root_file_name
 
     bins_to_root(bin_files, root_file_path)
-
- 
-

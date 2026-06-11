40b6f00deda3b29c60bfbaf84dff229438ccea85
Drew Ames <dreames21@gmail.com>
2026-06-11 10:08:26 -0400

    Plot channels in order:

--- other/fileperboard_root_plot_raw.py
+++ other/fileperboard_root_plot_raw.py
@@ -81,8 +81,6 @@ for event_num in tqdm(range(total_events_in_acq)):
     ax_csp_y.yaxis.tick_right()
     ax_csp_y.yaxis.set_label_position("right")
 
-    offset_value_x = 0
-    offset_value_y = 0 # this isn't needed right now since we only have one axis
     offset_bin = 25
     # ------ end of JC's code snippet -------
 
@@ -93,15 +91,16 @@ for event_num in tqdm(range(total_events_in_acq)):
     ax_csp_x.vlines(16, -50, 550, ls="--", color="black", alpha=0.8)
     ax_csp_y.vlines(16, -50, 550, ls="--", color="black", alpha=0.8)
 
-    # catalog for light and charge channels - sourced from xwiki
-    light_vis_cat = [32, 33] # odd suffixes in description,  CAEN CHANNEL NUMBERS
-    light_vuv_cat = [30, 31] # even suffixes in description, CAEN CHANNEL NUMBERS
-    charge_cat_x = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48] # even suffixes in description, CAEN CHANNEL NUMBERS
-    charge_cat_y = [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]
+    light_vis_cat = [2]
+    light_vuv_cat=[]
+
+    # using DB50->2xDB37
+    charge_cat_x = [21, 5, 39, 22, 6, 40, 23, 7, 41, 24, 8, 42, 25, 9, 26] # B
+    charge_cat_y = [27, 10, 44, 28, 11, 45, 29, 12, 46, 30, 13, 47, 31, 14, 48] #T
 
     event_id = event_obj.event_num[0]
 
-    for chan in waveform_data: # for key in dict
+    for chan in (charge_cat_x + charge_cat_y):
 
         # all waveforms are baseline corrected
         base_corr_waveform = waveform_data[chan] - np.mean(waveform_data[chan][:1500]) # 1500 because of our pre-trigger time, this is hard-coded and needs to be modified for different pre-trigger times
@@ -109,27 +108,30 @@ for event_num in tqdm(range(total_events_in_acq)):
         #### checking for interesting events ####
 
         # start with charge channels
-        if (chan in charge_cat_x or chan in charge_cat_y):
-            # First, check if it's interesting
-            # ------ this bit from JC's code ------
-            rms = np.sqrt(np.mean(base_corr_waveform[:-int(event_obj.num_samples[0] / 3)] ** 2))
-            if np.mean(base_corr_waveform[2200:][np.argpartition(base_corr_waveform[2200:], -5000)[-5000:]]) - np.mean(base_corr_waveform[:1500]) > 4 * rms and rms > 1:
-                hit_channels += 1
-            # ------ end ------
+        # First, check if it's interesting
+        # ------ this bit from JC's code ------
+        rms = np.sqrt(np.mean(base_corr_waveform[:-int(event_obj.num_samples[0] / 3)] ** 2))
+        if np.mean(base_corr_waveform[2200:][np.argpartition(base_corr_waveform[2200:], -5000)[-5000:]]) - np.mean(base_corr_waveform[:1500]) > 4 * rms and rms > 1:
+            hit_channels += 1
+        # ------ end ------
 
-            # then plot on the x or y axes
-            if chan in charge_cat_x:
-                offset_base_corr_waveform = base_corr_waveform + offset_value_x
-                line, = ax_csp_x.plot(time_array, offset_base_corr_waveform, alpha=0.8) # Robin added alpha here to make lines translucent
-                offset_value_x += offset_bin
+        # then plot on the x or y axes
+        if chan in charge_cat_x:
+            idx = charge_cat_x.index(chan)
+            offset_value = offset_bin * idx
+            offset_base_corr_waveform = base_corr_waveform + offset_value
+            line, = ax_csp_x.plot(time_array, offset_base_corr_waveform, alpha=0.8)
 
-            elif chan in charge_cat_y:
-                offset_base_corr_waveform = base_corr_waveform + offset_value_y
-                line, = ax_csp_y.plot(time_array, offset_base_corr_waveform, alpha=0.8)
-                offset_value_y += offset_bin
-        
+        elif chan in charge_cat_y:
+            idx = charge_cat_y.index(chan)
+            offset_value = offset_bin * idx
+            offset_base_corr_waveform = base_corr_waveform + offset_value
+            line, = ax_csp_y.plot(time_array, offset_base_corr_waveform, alpha=0.8)
+
+    for chan in light_vis_cat + light_vuv_cat:
         # moving on to light channels
-        # ------ code from JC ------
+        base_corr_waveform = waveform_data[chan] - np.mean(waveform_data[chan][:1500]) # 1500 because of our pre-trigger time, this is hard-coded and needs to be modified for different pre-trigger times
+
         if chan in light_vis_cat:
             line = ax_sipm_vis.plot(time_array, base_corr_waveform, alpha=0.8)[0]  # Note [0] here <-- JC's comment
         elif chan in light_vuv_cat:
